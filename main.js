4d20b98add14bf180bbce476d6d99383192c5e5b
TWOdunlami <t_alvarez88@hotmail.com>
2022-12-10 21:07:36 -0500

    draft madden champs landing page countdown.

---
+++ main.js
@@ -0,0 +1,21 @@
+// Adjuster Trainee start date
+var countDownDate = new Date("December 27, 2022 08:30:00").getTime();
+
+var ramos = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 *60)) / 1000);
+
+    document.getElementById("tania").innerHTML = days + "d " + hours + "h " 
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(ramos);
+        document.getElementById("tania").innerHTML = "Today is my first day working for Progressive Insurance, as a Claims Adjuster Trainee! Thank you for the 💌 kind & motivational messages.";
+    }
+
+},
+    1000)
