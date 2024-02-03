915fc5c81dc23c1ba78c6d34517755800fdeee0e
TWOdunlami <t_alvarez88@hotmail.com>
2024-02-03 18:19:05 -0500

    leaderboard page for regular season ranking and playoffs.

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-// Adjuster Trainee start date
-var countDownDate = new Date("February 27, 2024 08:30:00").getTime();
+    // Regular season start date
+var countDown = new Date("February 4, 2024 08:30:00").getTime();
+
+var playyoffs = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDown - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 *60)) / 1000);
+
+    document.getElementById("regszn").innerHTML = days + "d " + hours + "h " 
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(playyofff);
+        document.getElementById("regszn").innerHTML = "PLAYOFF MODE! Let the best 1 win!";
+    }
+
+},
+    1000)
+
+    // Playoffs start date
+var countDownDate = new Date("February 24, 2024 08:30:00").getTime();
 
 var ramos = setInterval(function() {
     var now = new Date().getTime();
@@ -9,12 +31,12 @@ var ramos = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 *60)) / 1000);
 
-    document.getElementById("dami").innerHTML = days + "d " + hours + "h " 
+    document.getElementById("playoffs").innerHTML = days + "d " + hours + "h " 
     + minutes + "m " + seconds + "s ";
 
     if (distance < 0) {
         clearInterval(ramos);
-        document.getElementById("dami").innerHTML = "Let the Games Begin!";
+        document.getElementById("playoffs").innerHTML = "Let the Games Begin!";
     }
 
 },
